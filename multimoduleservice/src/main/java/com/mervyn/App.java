644cbe07f1e0ef1daacec69208cf91bb189dd761
贺鑫鑫 <hexinxin@quyiyuan.com>
2017-08-21 19:45:16 +0800

    Initial commit

---
+++ multimoduleservice/src/main/java/com/mervyn/App.java
@@ -0,0 +1,13 @@
+package com.mervyn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
